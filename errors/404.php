cc17b1d9f625d577739854cd03419da047ee198e
markorusic <markousp5@gmail.com>
2018-03-10 18:04:21 +0100

    http errors update

---
+++ errors/404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Greška</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<div class="flex-center-col mt-5">
+		<h3>Greška 404! Stranica nije pronadjena.</h3>
+		<a href="/">Vrati se nazad</a>
+	</div>
+</body>
+</html>
